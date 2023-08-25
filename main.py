13c2b30d1c324d6d12c880131b6c9ebd45811076
thinh-huynh-re <thinh.researcher@gmail.com>
2023-08-25 13:48:30 +0700

    Init

---
+++ main.py
@@ -0,0 +1,23 @@
+from jsonformer import Jsonformer
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model = AutoModelForCausalLM.from_pretrained(
+    "databricks/dolly-v2-12b", cache_dir="cache"
+)
+tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-12b", cache_dir="cache")
+
+json_schema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "age": {"type": "number"},
+        "is_student": {"type": "boolean"},
+        "courses": {"type": "array", "items": {"type": "string"}},
+    },
+}
+
+prompt = "Generate a person's information based on the following schema:"
+jsonformer = Jsonformer(model, tokenizer, json_schema, prompt)
+generated_data = jsonformer()
+
+print(generated_data)
